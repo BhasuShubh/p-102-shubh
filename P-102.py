76139fef0fbaab2bd17b1ae8b8ffac1691e60d8c
BhasuShubh <78732766+BhasuShubh@users.noreply.github.com>
2022-10-08 09:39:53 +0530

    Add files via upload

---
+++ P-102.py
@@ -0,0 +1,32 @@
+from distutils import extension
+import os
+import shutil
+
+from_dir ="C:/Users/shubh/Downloads"
+to_dir = "C:/Users/shubh/OneDrive/Desktop/cluster"
+
+list_of_files = os.listdir(from_dir)
+
+for file_name in list_of_files :
+    name, extension = os.path.splitext(file_name) 
+
+    if extension == '' :
+        continue
+
+    if extension in [ ".txt", ".doc", ".docx", ".pdf"] :
+        path1 = from_dir + '/' + file_name
+        path2 = to_dir +'/' + "Documents"
+        path3 = to_dir + '/' + "Documents" + '/' + file_name 
+
+        if os.path.exists(path2) :
+            print("Moving " + file_name +  "....")
+
+
+            shutil.move(path1, path3)
+
+        else :
+            os.makedirs(path2)
+            print("moving "+ file_name +"....")
+
+            shutil.move(path1, path3)
+    
